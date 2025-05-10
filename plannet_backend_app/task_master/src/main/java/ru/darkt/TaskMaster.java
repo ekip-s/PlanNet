6f5c6e5856d3fb5619b83092aa9c6d0fb7effe7a
hellcherry <ekip-s@yandex.ru>
2025-05-10 17:47:45 +0300

    add backend app

---
+++ plannet_backend_app/task_master/src/main/java/ru/darkt/TaskMaster.java
@@ -0,0 +1,11 @@
+package ru.darkt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskMaster {
+    public static void main(String[] args) {
+        SpringApplication.run(TaskMaster.class, args);
+    }
+}
