4ffd81fcfb512c5e6a1d8110422bf4e1b482099b
hellcherry <ekip-s@yandex.ru>
2025-05-12 21:49:42 +0300

    fix

--- plannet_backend_app/task_master/src/main/java/ru/darkt/conf/SecurityConfig.java
+++ plannet_backend_app/task_master/src/main/java/ru/darkt/conf/SecurityConfig.java
@@ -74,6 +74,7 @@ public class SecurityConfig {
                         .requestMatchers(
                                 "/swagger-ui/index.html",
                                 "/swagger-ui/**",
+                                "/v3/api-docs",
                                 "/v3/api-docs/**"
                         ).permitAll()
                         .anyRequest().authenticated()
